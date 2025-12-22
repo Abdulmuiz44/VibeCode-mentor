bb0f975de3eb17ae5cd652b38b44f5aa5c8f9dab
Abdulmuiz44 <abdulmuizproject@gmail.com>
2025-12-22 13:05:31 +0100

    chore: remove all PayPal and Flutterwave integration files

--- types/flutterwave.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'flutterwave-node-v3' {
-  export default class Flutterwave {
-    constructor(publicKey: string, secretKey: string);
-    Charge: {
-      card(payload: any): Promise<any>;
-    };
-  }
-}
