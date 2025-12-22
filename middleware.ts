0c2292162f8ef4ccdc194f6008606caa609a2e65
Abdulmuiz44 <abdulmuizproject@gmail.com>
2025-12-22 16:26:56 +0100

    Fix middleware matcher pattern to allow static assets and homepage to load

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,3 @@
-import { withAuth } from 'next-auth/middleware';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
@@ -17,5 +16,13 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/:path*'],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!_next/static|_next/image|favicon.ico).*)',
+  ],
 };
